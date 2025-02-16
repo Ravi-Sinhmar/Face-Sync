40aacdc8b8a41f9c96dc329a5d2f00e2a0fea56e
Ravi-Sinhmar <engineerfuture82@gmail.com>
2025-02-16 20:46:37 +0530

    updated all urls

--- client/src/Contexts/Socket.jsx
+++ client/src/Contexts/Socket.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("https://facesyncbackend.onrender.com"), []);
+  const socket = useMemo(() => io("https://face-sync.onrender.com"), []);
 
   
 
